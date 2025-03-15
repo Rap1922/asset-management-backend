47f85a5e92a9048fb0530567b6cfed282e091535
Rap1922 <riyanagungpratama@gmail.com>
2025-03-16 02:22:46 +0700

    Fix Server 2

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get("/", (req, res) => {
 });
 
 // 🔹 Jalankan Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT} and accessible via network`);
 });
